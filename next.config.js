81128c6c95d185822fe6a22e0b7ba1c8e929dcc6
emmajane1313 <admin@digitalax.xyz>
2022-10-20 08:12:14 -0400

    init repo

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  experimental: {
+    scrollRestoration: true,
+    optimizeCss: true,
+  },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
 }
 
 module.exports = nextConfig
